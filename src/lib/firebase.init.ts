8eb832bf9095588ec25676110b0f0cc2717552a3
unknown <mdomarfaruk149518@gmail.com>
2024-08-08 23:10:01 +0600

    check

--- src/lib/firebase.init.ts
+++ src/lib/firebase.init.ts
@@ -1,7 +1,7 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth'
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyA9Ps_m6jI2VizyDmnEi4l_8vIkupeGUXU",
   authDomain: "react-therapy.firebaseapp.com",
@@ -11,7 +11,7 @@ const firebaseConfig = {
   appId: "1:765611184295:web:1bef56073222f20c0c8b82"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);;
